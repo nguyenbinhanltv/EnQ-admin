3560748133132eb75cf6c01dd579743f7584f3bd
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-16 22:24:09 +0700

    update sidebar

---
+++ Data/MenuItem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnQ_admin.Data
+{
+    public class MenuItem
+    {
+        public string pageName { get; set; }
+        public string routerLink { get; set; }
+        public string iconName { get; set; }
+    }
+}
